75d929e66ce680fcf6afca1ef5cf29fdf54e1e84
David <githubthrowaway12957@yandex.ru>
2016-12-20 05:24:12 +0800

    create index.php

---
+++ web/index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+	<script src='jquery-3.1.1.js'></script>
+</head>
+<body>
+	<center>
+		<div id='div1' style='text-align: left; outline: 2px solid black;'>
+		</div>
+		<br>
+		<br>
+		<a id='button' download>Download text file in current state</a>
+	</center>
+	<script type='text/javascript'>
+	dates = new Date()
+	month = dates.getMonth() + 1 
+	download = 'logs/' + dates.getFullYear() + ':' +  month +  ':' + dates.getDate() + '.txt'
+	setInterval(function(){ $.ajax({url: "output.php", success: function(result){
+        				$("#div1").html(result);
+    			}}); }, 50);
+
+	$("a").attr("href", download)
+	</script>
+</body>
+</html>
